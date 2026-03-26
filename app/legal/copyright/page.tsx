0540ecb3b100fe66ebfe22ca12b2474953fb6ff9
Zia269100 <md.ziakhalid2691@gmail.com>
2026-03-26 17:48:51 +0530

    ddfnh

--- app/legal/copyright/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Copyright() {
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">Copyright</h1>
-      <p>© 2026 Glyph. All rights reserved.</p>
-    </div>
-  );
