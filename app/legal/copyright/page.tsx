6f1725478a69fe1e855e6ce0726d400dd070e284
Zia269100 <md.ziakhalid2691@gmail.com>
2026-03-26 17:32:17 +0530

    sfv

---
+++ app/legal/copyright/page.tsx
@@ -0,0 +1,12 @@
+export default function CopyrightPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Copyright</h1>
+      <p>© 2026 Mohammed Zia. All rights reserved.</p>
+      <p>
+        This project (Glyph) is a prototype for emergency medical identification
+        using NFC technology.
+      </p>
+    </div>
+  );
+}
